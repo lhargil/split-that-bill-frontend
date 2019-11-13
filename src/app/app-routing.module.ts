e218cbc3f66b3e4a1fbcaf74eb9260aac5f139dc
Lhar Gil <devlana@outlook.com>
2019-11-13 21:13:47 +0800

    feat: site skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [{ path: 'bills', loadChildren: () => import('./bills/bills.module').then(m => m.BillsModule) }, { path: 'people', loadChildren: () => import('./people/people.module').then(m => m.PeopleModule) }, { path: 'billing', loadChildren: () => import('./billing/billing.module').then(m => m.BillingModule) }, { path: 'accounts', loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule) }];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { path: 'bills', loadChildren: () => import('./bills/bills.module').then(m => m.BillsModule) }, { path: 'people', loadChildren: () => import('./people/people.module').then(m => m.PeopleModule) }, { path: 'billing', loadChildren: () => import('./billing/billing.module').then(m => m.BillingModule) }, { path: 'accounts', loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule) },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full' 
+  },
+{ path: '**', component: PageNotFoundComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
