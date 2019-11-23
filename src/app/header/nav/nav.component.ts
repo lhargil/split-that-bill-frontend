6fc05f7fa15ae48b30fc0be6573f65b8044912f6
Lhar Gil <devlana@outlook.com>
2019-11-23 13:14:46 +0800

    refactor: header nav active route style

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { merge, fromEvent, BehaviorSubject, ReplaySubject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { map, takeUntil, filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit, OnDestroy {
+export class NavComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('siteNav', {static: false}) siteNav: ElementRef;
   @ViewChild('siteTitle', {static: false}) siteTitle: ElementRef;
   @ViewChild('siteNavTrigger', {static: false}) siteNavTrigger: ElementRef;
@@ -22,13 +23,13 @@ export class NavComponent implements OnInit, OnDestroy {
   );
 
   visible = false;
-  toggleClass = 'from-right';
 
-  constructor(private renderer2: Renderer2) { }
+  constructor(private renderer2: Renderer2,
+    private router: Router) { }
 
   ngOnInit() {
     this.scrolling$
-      .subscribe(() => this.onScroll())
+      .subscribe(() => this.onScroll())    
   }
 
   ngOnDestroy() {
@@ -36,27 +37,52 @@ export class NavComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
+  ngAfterViewInit() {
+    this.router.events
+      .pipe(
+        takeUntil(this.destroy$),
+        filter(ev => ev instanceof NavigationEnd)
+      ).subscribe(ev => {
+        const activeUrl = (ev as NavigationEnd).url;
+        if (activeUrl == '/home' || activeUrl == '/') {
+          this.shadowOff();
+        } else {
+          this.shadowOn();
+        }
+      });
+  }
+  
   onScroll() {
     const scrollY = window.scrollY || window.pageYOffset;
-
+    
     if (scrollY > 0) {
-      this.renderer2.addClass(this.siteNav.nativeElement, 'bg-white');
-      this.renderer2.addClass(this.siteNav.nativeElement, 'shadow-md');
-      this.renderer2.addClass(this.siteTitle.nativeElement, 'text-gray-800');
-      this.renderer2.addClass(this.siteNavTrigger.nativeElement, 'text-gray-800');
+      this.shadowOn();
     } else {
-      this.renderer2.removeClass(this.siteNav.nativeElement, 'bg-white');
-      this.renderer2.removeClass(this.siteNav.nativeElement, 'shadow-md');
-      this.renderer2.removeClass(this.siteTitle.nativeElement, 'text-gray-800');
-      this.renderer2.removeClass(this.siteNavTrigger.nativeElement, 'text-gray-800');
+      this.shadowOff();
     }
   }
-
+  
   hideMenu(invisible: boolean) {
     this.visible = invisible;
   }
-
+  
   showMenu() {
     this.visible = true;
+  }
+  
+  private shadowOff() {
+    this.renderer2.removeClass(this.siteNav.nativeElement, 'bg-white');
+    this.renderer2.removeClass(this.siteNav.nativeElement, 'shadow-md');
+    this.renderer2.addClass(this.siteNav.nativeElement, 'text-white');
+    this.renderer2.removeClass(this.siteTitle.nativeElement, 'text-gray-800');
+    this.renderer2.removeClass(this.siteNavTrigger.nativeElement, 'text-gray-800');
+  }
+
+  private shadowOn() {
+    this.renderer2.addClass(this.siteNav.nativeElement, 'bg-white');
+    this.renderer2.addClass(this.siteNav.nativeElement, 'shadow-md');
+    this.renderer2.addClass(this.siteTitle.nativeElement, 'text-gray-800');
+    this.renderer2.removeClass(this.siteNav.nativeElement, 'text-white');
+    this.renderer2.addClass(this.siteNavTrigger.nativeElement, 'text-gray-800');
   }
 }
