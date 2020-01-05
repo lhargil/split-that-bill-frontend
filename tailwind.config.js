76a5b18c4ffe46e498dd8f643d42a1fb7a89f045
Lhar Gil <devlana@outlook.com>
2020-01-06 04:59:02 +0800

    chore: configuration update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    fontFamily: {
+      display: ['Raleway', 'Roboto', 'sans-serif'],
+      body: ['Roboto', 'sans-serif'],
+    },
+    minWidth: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+    },
     extend: {
       inset: {
         '1/2': '50%',
