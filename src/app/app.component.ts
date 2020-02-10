dffcb14ce5d78e91da0af5fd102d52a95b11a21c
Lhar Gil <devlana@outlook.com>
2020-02-10 23:49:35 +0800

    fix: 100vh solution for ios safari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy, ViewChild, ComponentFactoryResolver } from '@angular/core';
 import { Router, NavigationStart, RouteConfigLoadStart, RouteConfigLoadEnd, NavigationEnd, NavigationCancel } from '@angular/router';
-import { tap, takeUntil } from 'rxjs/operators';
+import { tap, takeUntil, debounceTime } from 'rxjs/operators';
 import { LoaderService } from './shared/loader/loader.service';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, fromEvent } from 'rxjs';
 import { AuthService } from './core/auth/auth.service';
 import { AppService } from './app.service';
 import { ContentHostDirective } from './shared/directives/content-host/content-host.directive';
@@ -17,6 +17,8 @@ import { PersonEditorShellComponent } from './billing/person-editor-shell/person
 export class AppComponent implements OnInit, OnDestroy {
   private destroyed$ = new ReplaySubject(0);
   title = 'splitthatbill';
+
+
 
   constructor(private router: Router, private loaderService: LoaderService, public authService: AuthService, private appService: AppService) { }
   @ViewChild(ContentHostDirective, { static: true }) contentHost: ContentHostDirective;
@@ -36,10 +38,25 @@ export class AppComponent implements OnInit, OnDestroy {
     this.appService.getStarted$
       .pipe(takeUntil(this.destroyed$))
       .subscribe(_ => this.router.navigate(['/billing']));
+
+    fromEvent(window, 'resize')
+      .pipe(
+        debounceTime(200),
+        takeUntil(this.destroyed$),
+      ).subscribe(
+        _ => this.resetHeight()
+      );
+    this.resetHeight();
   }
 
   ngOnDestroy() {
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+
+  // this is a solution to handle the 100vh element height in IOS Safari as the browser bottom navigation covers up a portion of the bottom part of an element
+  // see more: https://stackoverflow.com/questions/43575363/css-100vh-is-too-tall-on-mobile-due-to-browser-ui
+  private resetHeight() {
+    document.body.style.height = window.innerHeight + 'px';
   }
 }
