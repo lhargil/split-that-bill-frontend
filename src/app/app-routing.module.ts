e7d3bd75a0e0d46da521f286c60eb596e4053989
Lhar Gil <devlana@outlook.com>
2020-02-28 23:18:25 +0800

    feat: added about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,6 +41,7 @@ const routes: Routes = [
     redirectTo: '/home',
     pathMatch: 'full'
   },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: '**', component: PageNotFoundComponent },
 ];
 
