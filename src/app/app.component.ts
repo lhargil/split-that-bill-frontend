5d91178d773ddd7854a825df108fa15bbf92d7ee
Lhar Gil <devlana@outlook.com>
2020-03-09 01:23:28 +0800

    refactor: moved meta loading to app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,33 @@ export class AppComponent implements OnInit, OnDestroy {
   private destroyed$ = new ReplaySubject(0);
   title = 'splitthatbill';
 
-  constructor(private router: Router, private loaderService: LoaderService, private appService: AppService) { }
+  constructor(private router: Router, private loaderService: LoaderService, private appService: AppService, private meta: Meta) {
+    this.meta.addTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.addTag({ name: 'twitter:site', content: '@splitthatbill' });
+    this.meta.addTag({ name: 'twitter:title', content: 'Split that bill' });
+    this.meta.addTag({ name: 'twitter:description', content: 'An easy-to-use bill splitter app powered by Angular and ASP.NET CORE.' });
+    this.meta.addTag({ name: 'twitter:image', content: `${environment.siteUrl}/assets/images/thumbnail.png` });
+    this.meta.addTags([
+      { name: 'robots', content: 'INDEX, FOLLOW' },
+      { name: 'author', content: 'lhar santillan gil' },
+      { name: 'keywords', content: 'TypeScript, Angular, ASP.NET CORE, dotnet, Angular Universal, Prerender, csharp, bill splitter, split that bill, easy split' },
+      { name: 'date', content: '2020-03-08', scheme: 'YYYY-MM-DD' },
+      { httpEquiv: 'Content-Type', content: 'text/html' },
+      { property: 'og:title', content: 'Split that bill' },
+      { property: 'og:type', content: 'website' },
+      {
+        property: 'og:url', content: `${environment.siteUrl}`
+      },
+      {
+        property: 'og:description', content: 'An easy-to-use bill splitter app powered by Angular and ASP.NET CORE.'
+      },
+      {
+        property: 'og:site_name', content: 'splitthatbill app'
+      },
+      { property: 'og:image', content: `${environment.siteUrl}/assets/images/thumbnail.png` },
+      { charset: 'UTF-8' }
+    ]);
+  }
   @ViewChild(ContentHostDirective, { static: true }) contentHost: ContentHostDirective;
 
   ngOnInit() {
