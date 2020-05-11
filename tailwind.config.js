ce631ac9f7248a0dbb0e99523b9c9c277c3e9099
Lhar Gil <devlana@outlook.com>
2020-05-11 15:48:11 +0800

    feat(custom forms): added tw styles for custom forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Raleway', 'Roboto', ...defaultTheme.fontFamily.sans]
+        display: ['Raleway', ...defaultTheme.fontFamily.sans],
+        body: ['Roboto', ...defaultTheme.fontFamily.sans],
       },
       inset: {
         '1/2': '50%',
@@ -12,7 +13,21 @@ module.exports = {
       screens: {
         'xxs': '325px',
         'xs': '375px'
-      }
+      },
+      customForms: theme => ({
+        invalid: {
+          'input, textarea, select, multiselect, checkbox, radio': {
+            borderColor: theme('colors.red.300'),
+            '&::placeholder': {
+              color: theme('colors.red.300')
+            },
+            '&:focus': {
+              boxShadow: theme('boxShadow.outline-red'),
+              borderColor: theme('colors.red.300')
+            }
+          },
+        }
+      })
     },
   },
   variants: {
